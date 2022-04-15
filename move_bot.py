08e6df04f94376d05d9b3520175e92fcf95ac1af
meisnate12 <meisnate12@gmail.com>
2022-04-15 18:24:55 -0400

    fix optional message when replying

--- move_bot.py
+++ move_bot.py
@@ -93,6 +93,7 @@ async def on_message(msg_in):
 
     guild_id = msg_in.guild.id
     txt_channel = msg_in.channel
+    is_reply = msg_in.reference is not None
     if msg_in.content.startswith(f'{LISTEN_TO} reset'):
         with sqlite3.connect("settings.db") as connection:
             connection.row_factory = sqlite3.Row
@@ -102,7 +103,7 @@ async def on_message(msg_in):
             prefs.pop(guild_id)
         await txt_channel.send("All preferences reset to default")
     elif msg_in.content.startswith(LISTEN_TO):
-        params = msg_in.content.split(maxsplit=3)
+        params = msg_in.content.split(maxsplit=2 if is_reply else 3)
 
         # !mv help
         if len(params) < 2 or params[1] == 'help':
@@ -148,56 +149,49 @@ async def on_message(msg_in):
         # !mv [msgID] [#channel] [optional message]
         else:
             if msg_in.author.guild_permissions.manage_messages:
-                error_msg = ''
-                channel_param = 2
+                channel_param = 1 if is_reply else 2
+                extra_param = channel_param + 1
                 try:
-                    if msg_in.reference is not None:
-                        moved_msg = await txt_channel.fetch_message(msg_in.reference.message_id)
-                        channel_param = 1
-                    else:
-                        moved_msg = await txt_channel.fetch_message(params[1])
+                    moved_msg = await txt_channel.fetch_message(msg_in.reference.message_id if is_reply else params[1])
                 except:
-                    error_msg = error_msg + 'An invalid message ID was provided. You can ignore the message ID by executing the **move** command as a reply to the target message'
-
-                try:
-                    target_channel = msg_in.guild.get_channel_or_thread(int(params[channel_param].strip('<#').strip('>')))
-                except:
-                    error_msg = error_msg + "An invalid channel was provided. "
-
-                if error_msg:
-                    await txt_channel.send(error_msg)
+                    await txt_channel.send('An invalid message ID was provided. You can ignore the message ID by executing the **move** command as a reply to the target message')
                 else:
-                    r = Route('POST', '/channels/{channel_id}/webhooks', channel_id=target_channel.parent_id if isinstance(target_channel, Thread) else target_channel.id)
-                    data = await target_channel._state.http.request(r, json={'name': str(moved_msg.author.display_name)})
-                    wb = Webhook.from_state(data, state=target_channel._state)
-                    files = []
-                    for file in moved_msg.attachments:
-                        f = io.BytesIO()
-                        await file.save(f)
-                        files.append(discord.File(f, filename=file.filename))
-
-                    if isinstance(target_channel, Thread):
-                        await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar, embeds=moved_msg.embeds, files=files, thread=target_channel)
+                    try:
+                        target_channel = msg_in.guild.get_channel_or_thread(int(params[channel_param].strip('<#').strip('>')))
+                    except:
+                        await txt_channel.send("An invalid channel or thread was provided.")
                     else:
-                        await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar, embeds=moved_msg.embeds, files=files)
-                    await wb.delete()
+                        r = Route('POST', '/channels/{channel_id}/webhooks', channel_id=target_channel.parent_id if isinstance(target_channel, Thread) else target_channel.id)
+                        data = await target_channel._state.http.request(r, json={'name': str(moved_msg.author.display_name)})
+                        wb = Webhook.from_state(data, state=target_channel._state)
+                        files = []
+                        for file in moved_msg.attachments:
+                            f = io.BytesIO()
+                            await file.save(f)
+                            files.append(discord.File(f, filename=file.filename))
 
-                    extra = f'\n\n{params[channel_param + 1]}' if len(params) > channel_param + 1 else ''
-                    if guild_id in prefs and "move_message" in prefs[guild_id] and prefs[guild_id]["move_message"]:
-                        notice_msg = prefs[guild_id]["move_message"]
-                    else:
-                        notice_msg = default_msg
-                    notice_msg = notice_msg.replace("MESSAGE_USER", f"<@!{moved_msg.author.id}>") \
-                        .replace("DESTINATION_CHANNEL", params[channel_param]) \
-                        .replace("MOVER_USER", f"<@!{msg_in.author.id}>")
+                        if isinstance(target_channel, Thread):
+                            await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar, embeds=moved_msg.embeds, files=files, thread=target_channel)
+                        else:
+                            await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar, embeds=moved_msg.embeds, files=files)
+                        await wb.delete()
 
-                    notice_msg = f'{notice_msg}{extra}'
-                    if guild_id in prefs and "notify_dm" in prefs[guild_id] and prefs[guild_id]["notify_dm"] == "1":
-                        await msg_in.author.send(notice_msg)
-                    else:
-                        await txt_channel.send(notice_msg)
-                    await msg_in.delete()
-                    await moved_msg.delete()
+                        extra = f'\n\n{params[extra_param]}' if len(params) > extra_param else ''
+                        if guild_id in prefs and "move_message" in prefs[guild_id] and prefs[guild_id]["move_message"]:
+                            notice_msg = prefs[guild_id]["move_message"]
+                        else:
+                            notice_msg = default_msg
+                        notice_msg = notice_msg.replace("MESSAGE_USER", f"<@!{moved_msg.author.id}>") \
+                            .replace("DESTINATION_CHANNEL", params[channel_param]) \
+                            .replace("MOVER_USER", f"<@!{msg_in.author.id}>")
+
+                        notice_msg = f'{notice_msg}{extra}'
+                        if guild_id in prefs and "notify_dm" in prefs[guild_id] and prefs[guild_id]["notify_dm"] == "1":
+                            await msg_in.author.send(notice_msg)
+                        else:
+                            await txt_channel.send(notice_msg)
+                        await msg_in.delete()
+                        await moved_msg.delete()
 
 #end
 client.run(TOKEN)
