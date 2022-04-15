32684ee1f9dffc92fc8d81c28ea14ad1837b2efe
meisnate12 <meisnate12@gmail.com>
2022-04-15 00:37:21 -0400

    work with threads

--- move_bot.py
+++ move_bot.py
@@ -3,6 +3,9 @@ import io
 import json
 import discord
 import requests
+from discord import Thread
+from discord.http import Route
+from discord.webhook import Webhook
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,8 +18,8 @@ BOT_ID = os.getenv('MOVEBOT_ID')
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 client = discord.Client(intents=intents)
-
 
 @client.event
 async def on_ready():
@@ -24,7 +27,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'for spoilers | {LISTEN_TO} help'))
 
     global admin
-    admin = await client.fetch_user(user_id=ADMIN_ID)
+    admin = await client.fetch_user(int(ADMIN_ID))
 
 @client.event
 async def on_guild_join(guild):
@@ -69,7 +72,7 @@ async def on_message(msg_in):
         params = msg_in.content.split()
 
         # !mv help
-        if params[1] == 'help':
+        if len(params) < 2 or params[1] == 'help':
             e = discord.Embed(title="MoveBot Help")
             e.description = \
                 "This bot can move messages in two different ways.\n" + \
@@ -96,22 +99,26 @@ async def on_message(msg_in):
                     error_msg = error_msg + 'An invalid message ID was provided. You can ignore the message ID by executing the **move** command as a reply to the target message'
 
                 try:
-                    target_channel = client.get_channel(int(params[channel_param].strip('<#').strip('>')))
+                    target_channel = msg_in.guild.get_channel_or_thread(int(params[channel_param].strip('<#').strip('>')))
                 except:
                     error_msg = error_msg + "An invalid channel was provided. "
 
-                if len(error_msg) != 0:
+                if error_msg:
                     await txt_channel.send(error_msg)
                 else:
-                    wb = await target_channel.create_webhook(name=moved_msg.author.display_name)
-
+                    r = Route('POST', '/channels/{channel_id}/webhooks', channel_id=target_channel.parent_id if isinstance(target_channel, Thread) else target_channel.id)
+                    data = await target_channel._state.http.request(r, json={'name': str(moved_msg.author.display_name)})
+                    wb = Webhook.from_state(data, state=target_channel._state)
                     files = []
                     for file in moved_msg.attachments:
                         f = io.BytesIO()
                         await file.save(f)
                         files.append(discord.File(f, filename=file.filename))
 
-                    await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar_url, embeds=moved_msg.embeds, files=files)
+                    if isinstance(target_channel, Thread):
+                        await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar, embeds=moved_msg.embeds, files=files, thread=target_channel)
+                    else:
+                        await wb.send(content=moved_msg.content, avatar_url=moved_msg.author.avatar, embeds=moved_msg.embeds, files=files)
                     await wb.delete()
 
                     notice_msg = f'<@!{moved_msg.author.id}>, your message has been moved to {params[channel_param]} by <@!{msg_in.author.id}>'
